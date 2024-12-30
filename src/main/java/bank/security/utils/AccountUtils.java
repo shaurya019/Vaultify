d4e951f7e8b698205081ede5c76346915136497a
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-31 01:04:04 +0530

    static part add

--- src/main/java/bank/security/utils/AccountUtils.java
+++ src/main/java/bank/security/utils/AccountUtils.java
@@ -1,4 +1,37 @@
 package bank.security.utils;
 
+import java.time.Year;
+
 public class AccountUtils {
+
+    public static final String ACCOUNT_EXISTS_CODE = "001";
+    public static final String ACCOUNT_EXISTS_MESSAGE = "This user already has an account Created!";
+    public static final String ACCOUNT_CREATION_SUCCESS = "002";
+    public static final String ACCOUNT_CREATION_MESSAGE = "Account has been successfully created!";
+    public static final String ACCOUNT_NOT_EXIST_CODE = "003";
+    public static final String ACCOUNT_NOT_EXIST_MESSAGE = "User with the provided Account Number does not exist";
+    public static final String ACCOUNT_FOUND_CODE = "004";
+    public static final String ACCOUNT_FOUND_SUCCESS = "User Account Found";
+    public static final String ACCOUNT_CREDITED_SUCCESS = "005";
+    public static final String ACCOUNT_CREDITED_SUCCESS_MESSAGE = "User Account was credited successfully";
+    public static final String INSUFFICIENT_BALANCE_CODE = "006";
+    public static final String INSUFFICIENT_BALANCE_MESSAGE = "Insufficient Balance";
+    public static final String ACCOUNT_DEBITED_SUCCESS = "007";
+    public static final String ACCOUNT_DEBITED_MESSAGE = "Account has been successfully debited";
+
+
+
+    public static String generateAccountNumber(){
+        Year curr = Year.now();
+        int min = 100000;
+        int max = 999999;
+    int randNumber = (int) Math.floor(Math.random() *(max-min+1)+min);
+
+    String year = String.valueOf(curr);
+        String randomNumber = String.valueOf(randNumber);
+        StringBuilder accountN = new StringBuilder();
+
+        return accountN.append(year).append(randomNumber).toString();
+    }
+
 }
